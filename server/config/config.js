31a5232f368b753634f9de03842ccb10ecb45316
JoseNavaro <jo.a.navarro.r@gmail.com>
2019-09-01 19:33:02 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://J0navarro:ex9f21yPSQgs8yYF@cluster0-04gnq.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
